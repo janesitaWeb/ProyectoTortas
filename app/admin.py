a92ee9f93b5c420554abdc7ff667f8630a1bfb48
janesitaWeb <luci.valderram@alumnos.duoc.cl>
2020-11-02 01:09:34 -0300

    subRama2.1

--- app/admin.py
+++ app/admin.py
@@ -6,9 +6,35 @@ from .models import Cliente, Tipo, Porciones, Producto, Pedido, Detalle
 
 
 #metodo que me permite listar todos los productos en admin
-#class ProductoAdmin(admin.ModelAdmin):
- #   list_display=("id","nombre","especificacion","precio_venta","stock_total","tipo")
-  #  search_fields=("id",)
+class ClienteAdmin(admin.ModelAdmin):
+      list_display=("nro_doc","tipo_doc","nombre_completo","fecha_nac","genero","telefono","email","pasword","direccion","ofertas")
+      search_fields=("nro_doc",)
+      list_filter=("tipo_doc",)  
+
+class TipoAdmin(admin.ModelAdmin):
+      list_display=("id","descripcion")
+      search_fields=("id",)
+      list_filter=("descripcion",)  
+
+class PorcionesAdmin(admin.ModelAdmin):
+      list_display=("id","cantidad","precio","cod_tipo")
+      search_fields=("id",)
+      list_filter=("cod_tipo",) 
+
+class ProductoAdmin(admin.ModelAdmin):
+      list_display=("id","nombre","descripcion","url","cod_tipo")
+      search_fields=("id",)
+      list_filter=("cod_tipo",) 
+
+class PedidoAdmin(admin.ModelAdmin):
+      list_display=("id","fec_ing","direccion","fec_entrega","hora_entrega","telefono","Recibe_nombre","cod_cliente")
+      search_fields=("id",)
+      date_hierarchy="fec_entrega"
+
+class DetalleAdmin(admin.ModelAdmin):
+      list_display=("id","cod_prod","cant","cod_porciones","cod_pedido")
+      search_fields=("cod_prod",)
+      list_filter=("cod_pedido",) 
 
 
 #class PromocionesAdmin(admin.ModelAdmin):
@@ -18,9 +44,9 @@ from .models import Cliente, Tipo, Porciones, Producto, Pedido, Detalle
 
 
 #registramos las clases en admin
-admin.site.register(Cliente)
-admin.site.register(Tipo)
-admin.site.register(Porciones)
-admin.site.register(Producto)
-admin.site.register(Pedido)
-admin.site.register(Detalle)
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Tipo, TipoAdmin)
+admin.site.register(Porciones, PorcionesAdmin)
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(Detalle, DetalleAdmin)
--- app/models.py
+++ app/models.py
@@ -7,11 +7,14 @@ class Cliente(models.Model):
         nombre_completo = models.CharField(max_length=50) 
         fecha_nac = models.DateField(max_length=20)                
         genero = models.CharField(max_length=20)
-        telefono = models.IntegerField(max_length=20)
+        telefono = models.IntegerField(default=0) 
         email = models.EmailField(max_length=50)
         pasword = models.CharField(max_length=10)
         direccion = models.CharField(max_length=250) 
         ofertas = models.BooleanField(max_length=1)
+
+        def __str__(self):
+            return self.nro_doc
 
 
 class Tipo(models.Model):
@@ -28,6 +31,9 @@ class Porciones(models.Model):
         precio = models.FloatField(max_length=20)
         cod_tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
 
+        def __str__(self):
+            return self.cantidad
+
 
 class Producto(models.Model):
         id = models.IntegerField(primary_key=True)
@@ -36,25 +42,29 @@ class Producto(models.Model):
         url = models.CharField(max_length=100)
         cod_tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
 
+        def __str__(self):
+            return self.nombre
+
 
 class Pedido(models.Model):       
         fec_ing = models.DateField(max_length=20)
         direccion = models.CharField(max_length=250)
         fec_entrega = models.DateField(max_length=20)
         hora_entrega = models.CharField(max_length=10)        
-        telefono = models.IntegerField(max_length=12) 
-        nombre_completo = models.CharField(max_length=50)    #para compras sin registro          
+        telefono = models.IntegerField(default=0) 
+        Recibe_nombre = models.CharField(max_length=50)    #para compras sin registro          
         cod_cliente = models.ForeignKey(Cliente,blank=True, null=True, on_delete=models.CASCADE) #null para compras sin registro         
 
+        
 
 class Detalle(models.Model):        
-        precio = models.FloatField(max_length=20)
         cod_prod = models.ForeignKey(Producto, on_delete=models.CASCADE)
-        cantidad = models.IntegerField 
+        cant = models.IntegerField(default=1) 
         cod_porciones = models.ForeignKey(Porciones, blank=True, null=True, on_delete=models.CASCADE)
         cod_pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
 
-
+        def __str__(self):
+            return self.cod_prod
 
 
 
